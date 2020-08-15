6698a1676f742a77d0b53c356453aca44cab812c
Stephen <squebe@gmail.com>
2020-08-14 21:10:47 -0600

    Add authenticated_rest_client fixture to inject tokens into rest client

--- rested/test/fixtures.py
+++ rested/test/fixtures.py
@@ -1,4 +1,5 @@
 import pytest
+from functools import wraps
 
 
 def database(transactional=False, reset_sequences=False, autouse=True):
@@ -8,6 +9,23 @@ def database(transactional=False, reset_sequences=False, autouse=True):
         if reset_sequences: assert transactional, "Reset sequences only works when transactional is True."
         helper(request, django_db_blocker, transactional=transactional, reset_sequences=reset_sequences)
     return db
+
+def authenticated_rest_client(autouse=True):
+    @pytest.fixture(autouse=True)
+    def authenticated_rest_client_fixture(rest, user):
+        def inject_api_token(f):
+            @wraps(f)
+            def wrapped(*args, **kwargs):
+                kwargs['X_API_KEY'] = user._token
+                return f(*args, **kwargs)
+            return wrapped
+
+        rest.get = inject_api_token(rest.get)
+        rest.put = inject_api_token(rest.put)
+        rest.post = inject_api_token(rest.post)
+        rest.delete = inject_api_token(rest.delete)
+        return rest
+    return authenticated_rest_client_fixture
 
 @pytest.fixture()
 def rest():
